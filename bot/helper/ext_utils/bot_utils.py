fbe4829d55310ae7b839f8b60bb6d6af418ec6fc
ERROR-404 <112565007+ItzHecker404@users.noreply.github.com>
2023-06-03 08:23:04 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -130,7 +130,7 @@ def progress_bar(pct):
     return p_str
 
 def get_readable_message():
-    msg = "<b>Powered by Luna</b>\n\n"
+    msg = "<b>Powered by Selti</b>\n\n"
     button = None
     STATUS_LIMIT = config_dict['STATUS_LIMIT']
     tasks = len(download_dict)
@@ -142,7 +142,7 @@ def get_readable_message():
         msg += f"<i>{escape(f'{download.name()}')}\nby {download.extra_details['source']}</i>\n\n"
         msg += f"<b>┌ {download.status()} with {download.engine}</b>"
         if download.status() not in [MirrorStatus.STATUS_SPLITTING, MirrorStatus.STATUS_SEEDING]:
-            msg += f"\n<b>├ <a href='https://github.com/5hojib/Luna'>{progress_bar(download.progress())}</a></b> {download.progress()}"
+            msg += f"\n<b>├ <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>{progress_bar(download.progress())}</a></b> {download.progress()}"
             msg += f"\n<b>├ </b>{download.processed_bytes()} of {download.size()}"
             msg += f"\n<b>├ Speed</b>: {download.speed()}"
             msg += f'\n<b>├ Estimated</b>: {download.eta()}'
